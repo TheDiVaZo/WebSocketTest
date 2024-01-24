716c4acf1e3447c798df3a26da9c86d940c24c9f
TheDiVaZo (server) <thedivazoyoutub@gmail.com>
2024-01-24 18:41:36 +0300

    Test

---
+++ src/main/java/me/thedivazo/test/websockettest/model/Message.java
@@ -0,0 +1,9 @@
+package me.thedivazo.test.websockettest.model;
+
+/**
+ * @author TheDiVaZo
+ * created on 24.01.2024
+ */
+public class Message {
+    String message;
+}
---
+++ src/test/java/me/thedivazo/test/websockettest/WebSocketTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.thedivazo.test.websockettest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
